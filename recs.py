b6c8ad9db34c4bf1890dcd5ee365833f7b4fc348
tarek <tsamhan@gmail.com>
2019-01-13 21:51:42 -0500

    added os line

--- recs.py
+++ recs.py
@@ -3,11 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import project_tests as t
 import pickle
+import os
+os.chdir(r"C:\Users\tarek\git_workspace\dsnd-recommendations-ibm")
 
-%matplotlib inline
-
-df = pd.read_csv('data/user-item-interactions.csv')
-df_content = pd.read_csv('data/articles_community.csv')
+df = pd.read_csv('user-item-interactions.csv')
+df_content = pd.read_csv('articles_community.csv')
 del df['Unnamed: 0']
 del df_content['Unnamed: 0']
 
